6818cb03315400597e643b3a07e09a0a2b408437
TellyHub <106040852+TellyHub@users.noreply.github.com>
2022-07-11 17:19:45 +0530

    Update button.py

--- Uploader/button.py
+++ Uploader/button.py
@@ -91,10 +91,17 @@ async def youtube_dl_call_back(bot, update):
     if "fulltitle" in response_json:
         description = response_json["fulltitle"][0:1021]
         # escape Markdown and special characters
-    tmp_directory_for_each_user = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + f'{random1}'
+    tmp_directory_for_each_user = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + f'{random1}
     if not os.path.isdir(tmp_directory_for_each_user):
         os.makedirs(tmp_directory_for_each_user)
-    download_directory = tmp_directory_for_each_user + "/" + custom_file_name
+    if '/' in custom_file_name:
+        file_mimx = custom_file_name
+        file_maix = file_mimx.split('/')
+        file_name = ' '.join(file_maix)
+    else:
+        file_name = custom_file_name
+    download_directory = tmp_directory_for_each_user + "/" + str(file_name)
+
     command_to_exec = []
    
     if tg_send_type == "audio":
