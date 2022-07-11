b2f7d69eaae16af8eb225aa3f31a0f4e508a5b58
TellyHub <106040852+TellyHub@users.noreply.github.com>
2022-07-11 13:51:09 +0530

    Update echo.py

--- Uploader/echo.py
+++ Uploader/echo.py
@@ -122,6 +122,9 @@ async def echo(bot, update):
     if youtube_dl_username is not None:
         command_to_exec.append("--username")
         command_to_exec.append(youtube_dl_username)
+    if Config.BYPASS != "":
+        command_to_exec.append("--geo-bypass-country")
+        command_to_exec.append(Config.BYPASS)
     if youtube_dl_password is not None:
         command_to_exec.append("--password")
         command_to_exec.append(youtube_dl_password)
