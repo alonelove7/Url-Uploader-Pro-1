7b05557ff128292ff52c12accec7076b20371474
TellyHub <106040852+TellyHub@users.noreply.github.com>
2022-07-11 12:01:21 +0530

    Update echo.py

--- Uploader/echo.py
+++ Uploader/echo.py
@@ -11,20 +11,20 @@ import requests, urllib.parse, filetype, os, time, shutil, tldextract, asyncio, 
 from PIL import Image
 from plugins.config import Config
 import time
-from plugins.script import Translation
+from Uploader.script import Translation
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 from pyrogram import filters
 from pyrogram import Client, enums
-from plugins.functions.forcesub import handle_force_subscribe
-from plugins.functions.display_progress import humanbytes
-from plugins.functions.help_uploadbot import DownLoadFile
-from plugins.functions.display_progress import progress_for_pyrogram, humanbytes, TimeFormatter
+from Uploader.functions.forcesub import handle_force_subscribe
+from Uploader.functions.display_progress import humanbytes
+from Uploader.functions.help_uploadbot import DownLoadFile
+from Uploader.functions.display_progress import progress_for_pyrogram, humanbytes, TimeFormatter
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import UserNotParticipant
-from plugins.functions.ran_text import random_char
-from plugins.database.add import add_user_to_database
+from Uploader.functions.ran_text import random_char
+from Uploader.database.add import add_user_to_database
 from pyrogram.types import Thumbnail
 
 @Client.on_message(filters.incoming & filters.regex('^(ht|f)tp*')
