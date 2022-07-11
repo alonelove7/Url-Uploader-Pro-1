588a94faa9eae6b802909bf169b91ba9143cb939
TellyHub <106040852+TellyHub@users.noreply.github.com>
2022-07-11 13:43:17 +0530

    Update button.py

--- Uploader/button.py
+++ Uploader/button.py
@@ -117,7 +117,7 @@ async def youtube_dl_call_back(bot, update):
             "yt-dlp",
             "-c",
             "--cookies", "cookies.txt",
-            "--geo-bypass-country IN",
+            "--geo-bypass-country", Config.BYPASS,
             
             "--clean-info-json",
             "--ignore-no-formats-error",
