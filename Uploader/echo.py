fa1f04c49f68225ca064f7002ba038343842a185
TellyHub <106040852+TellyHub@users.noreply.github.com>
2022-07-11 13:53:22 +0530

    Update echo.py

--- Uploader/echo.py
+++ Uploader/echo.py
@@ -114,7 +114,7 @@ async def echo(bot, update):
             "yt-dlp",
             "--no-warnings",
             "--cookies", "cookies.txt",
-            "--geo-bypass-country IN",
+            "--geo-bypass-country", Config.BYPASS,
             "--youtube-skip-hls-manifest",
             "-j",
             url
