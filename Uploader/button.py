fdf322c8c106aa394ebd18ad446495a627c15520
TellyHub <106040852+TellyHub@users.noreply.github.com>
2022-07-12 12:30:00 +0530

    Update button.py

--- Uploader/button.py
+++ Uploader/button.py
@@ -201,26 +201,8 @@ async def youtube_dl_call_back(bot, update):
             # ref: message from @Sources_codes
             start_time = time.time()
             # try to upload file
-            if tg_send_type == "audio":
-                duration = await Mdata03(download_directory)
-                thumbnail = await Gthumb01(bot, update)
-                await bot.send_audio(
-                    chat_id=update.message.chat.id,
-                    audio=download_directory,
-                    caption=description,
-                   # parse_mode="HTML",
-                    duration=duration,
-                    thumb=thumbnail,
-                    reply_to_message_id=update.message.reply_to_message.id,
-                    progress=progress_for_pyrogram,
-                    progress_args=(
-                        Translation.UPLOAD_START,
-                        update.message,
-                        #custom_file_name,
-                        start_time
-                    )
-                )
-            elif tg_send_type == "file":
+
+            if (await db.get_upload_as_doc(update.from_user.id)) is False:
                 thumbnail = await Gthumb01(bot, update)
                 await bot.send_document(
                     chat_id=update.message.chat.id,
@@ -237,24 +219,7 @@ async def youtube_dl_call_back(bot, update):
                         start_time
                     )
                 )
-            elif tg_send_type == "vm":
-                width, duration = await Mdata02(download_directory)
-                thumbnail = await Gthumb02(bot, update, duration, download_directory)
-                await bot.send_video_note(
-                    chat_id=update.message.chat.id,
-                    video_note=download_directory,
-                    duration=duration,
-                    length=width,
-                    thumb=thumb_image_path,
-                    reply_to_message_id=update.message.reply_to_message.id,
-                    progress=progress_for_pyrogram,
-                    progress_args=(
-                        Translation.UPLOAD_START,
-                        update.message,
-                        #custom_file_name,
-                        start_time
-                    )
-                )
+
             elif tg_send_type == "video":
                  width, height, duration = await Mdata01(download_directory)
                  thumbnail = await Gthumb02(bot, update, duration, download_directory)
@@ -277,7 +242,43 @@ async def youtube_dl_call_back(bot, update):
                         start_time
                     )
                 )
-            
+            if tg_send_type == "audio":
+                duration = await Mdata03(download_directory)
+                thumbnail = await Gthumb01(bot, update)
+                await bot.send_audio(
+                    chat_id=update.message.chat.id,
+                    audio=download_directory,
+                    caption=description,
+                   # parse_mode="HTML",
+                    duration=duration,
+                    thumb=thumbnail,
+                    reply_to_message_id=update.message.reply_to_message.id,
+                    progress=progress_for_pyrogram,
+                    progress_args=(
+                        Translation.UPLOAD_START,
+                        update.message,
+                        #custom_file_name,
+                        start_time
+                    )
+                ) 
+            elif tg_send_type == "vm":
+                width, duration = await Mdata02(download_directory)
+                thumbnail = await Gthumb02(bot, update, duration, download_directory)
+                await bot.send_video_note(
+                    chat_id=update.message.chat.id,
+                    video_note=download_directory,
+                    duration=duration,
+                    length=width,
+                    thumb=thumb_image_path,
+                    reply_to_message_id=update.message.reply_to_message.id,
+                    progress=progress_for_pyrogram,
+                    progress_args=(
+                        Translation.UPLOAD_START,
+                        update.message,
+                        #custom_file_name,
+                        start_time
+                    )
+                )           
             else:
                 logger.info("🎧 Did This Happen")
             end_two = datetime.now()
