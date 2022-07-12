64df63d18ed2ba1aa3f7524dcd54bd9459fd971e
TellyHub <106040852+TellyHub@users.noreply.github.com>
2022-07-12 13:44:57 +0530

    Update commands.py

--- Uploader/commands.py
+++ Uploader/commands.py
@@ -18,24 +18,28 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from Uploader.database.add import add_user_to_database
 from Uploader.functions.forcesub import handle_force_subscribe
 
-@Client.on_message(filters.command(["start"]) & filters.private)
-async def start(bot, update):
-    if not update.from_user:
-        return await update.reply_text("I don't know about you sar :(")
-    await add_user_to_database(bot, update)
-    await bot.send_message(
-        Config.LOG_CHANNEL,
-           f"#NEW_USER: \n\nNew User [{update.from_user.first_name}](tg://user?id={update.from_user.id}) started @{Config.BOT_USERNAME} !!"
-    )
-    
-    if Config.UPDATES_CHANNEL:
-      fsub = await handle_force_subscribe(bot, update)
-      if fsub == 400:
-        return
-    await update.reply_text(
-        text=Translation.START_TEXT.format(update.from_user.mention),
+
+@Client.on_message(
+    filters.command("start") & filters.private,
+)
+async def start_bot(_, m: Message):
+    return await m.reply_text(
+        Translation.START_TEXT.format(m.from_user.first_name),
+        reply_markup=Translation.START_BUTTONS,
         disable_web_page_preview=True,
-        reply_markup=Translation.START_BUTTONS
+        quote=True,
     )
+
+
+@Client.on_message(
+    filters.command("help") & filters.private,
+)
+async def help_bot(_, m: Message):
+    return await m.reply_text(
+        Translation.HELP_TEXT,
+        reply_markup=Translation.HELP_BUTTONS,
+        disable_web_page_preview=True,
+    )
+
 
 
