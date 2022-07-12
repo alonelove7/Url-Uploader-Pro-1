cda16c971547f214292382960cda32ee8ba1a3c4
TellyHub <106040852+TellyHub@users.noreply.github.com>
2022-07-12 12:45:44 +0530

    Update dl_button.py

--- Uploader/dl_button.py
+++ Uploader/dl_button.py
@@ -117,73 +117,78 @@ async def ddl_call_back(bot, update):
             start_time = time.time()
             if (await db.get_upload_as_doc(update.from_user.id)) is False:
                 thumbnail = await Gthumb01(bot, update)
-                await update.message.reply_document(
-                    #chat_id=update.message.chat.id,
+                await bot.send_document(
+                    chat_id=update.message.chat.id,
                     document=download_directory,
                     thumb=thumbnail,
                     caption=description,
-                    parse_mode=enums.ParseMode.HTML,
-                    #reply_to_message_id=update.id,
+                    #parse_mode="HTML",
+                    reply_to_message_id=update.message.reply_to_message.id,
                     progress=progress_for_pyrogram,
                     progress_args=(
                         Translation.UPLOAD_START,
                         update.message,
+                        #custom_file_name,
                         start_time
                     )
                 )
-            else:
+
+            elif tg_send_type == "video":
                  width, height, duration = await Mdata01(download_directory)
-                 thumb_image_path = await Gthumb02(bot, update, duration, download_directory)
-                 await update.message.reply_video(
-                    #chat_id=update.message.chat.id,
+                 thumbnail = await Gthumb02(bot, update, duration, download_directory)
+                 await bot.send_video(
+                    chat_id=update.message.chat.id,
                     video=download_directory,
                     caption=description,
+                    # parse_mode="HTML",
                     duration=duration,
                     width=width,
                     height=height,
+                    thumb=thumbnail,
                     supports_streaming=True,
-                    parse_mode=enums.ParseMode.HTML,
-                    thumb=thumb_image_path,
-                    #reply_to_message_id=update.id,
+                    reply_to_message_id=update.message.reply_to_message.id,
                     progress=progress_for_pyrogram,
                     progress_args=(
                         Translation.UPLOAD_START,
                         update.message,
+                        #custom_file_name,
                         start_time
                     )
                 )
             if tg_send_type == "audio":
                 duration = await Mdata03(download_directory)
                 thumbnail = await Gthumb01(bot, update)
-                await update.message.reply_audio(
-                    #chat_id=update.message.chat.id,
+                await bot.send_audio(
+                    chat_id=update.message.chat.id,
                     audio=download_directory,
                     caption=description,
-                    parse_mode=enums.ParseMode.HTML,
+                   # parse_mode="HTML",
                     duration=duration,
                     thumb=thumbnail,
-                    #reply_to_message_id=update.id,
+                    reply_to_message_id=update.message.reply_to_message.id,
                     progress=progress_for_pyrogram,
                     progress_args=(
                         Translation.UPLOAD_START,
                         update.message,
+                        #custom_file_name,
                         start_time
                     )
-                )
+                ) 
             elif tg_send_type == "vm":
                 width, duration = await Mdata02(download_directory)
                 thumbnail = await Gthumb02(bot, update, duration, download_directory)
-                await update.message.reply_video_note(
-                    #chat_id=update.message.chat.id,
+                await bot.send_video_note(
+                    chat_id=update.message.chat.id,
                     video_note=download_directory,
                     duration=duration,
                     length=width,
-                    thumb=thumbnail,
-                    #reply_to_message_id=update.id,
+                    thumb=thumb_image_path,
+                    reply_to_message_id=update.message.reply_to_message.id,
                     progress=progress_for_pyrogram,
                     progress_args=(
                         Translation.UPLOAD_START,
                         update.message,
+                        #custom_file_name,
                         start_time
                     )
                 )
