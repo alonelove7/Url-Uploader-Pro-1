1c9784ee5cfc1dded36bb8f9171d51dd615c15f0
TellyHub <106040852+TellyHub@users.noreply.github.com>
2022-07-13 10:28:30 +0530

    Update echo.py

--- Uploader/echo.py
+++ Uploader/echo.py
@@ -47,9 +47,7 @@ async def echo(bot, update):
             )
         except Exception as error:
             print(error)
-    if not update.from_user:
-        return await update.reply_text("I don't know about you sar :(")
-    await add_user_to_database(bot, update)
+
 
     if Config.UPDATES_CHANNEL:
       fsub = await handle_force_subscribe(bot, update)
