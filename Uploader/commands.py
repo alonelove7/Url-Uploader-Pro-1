59db34e589c617232c6af3236703ecd247c003a1
TellyHub <106040852+TellyHub@users.noreply.github.com>
2022-07-13 14:04:59 +0530

    Update commands.py

--- Uploader/commands.py
+++ Uploader/commands.py
@@ -87,8 +87,12 @@ async def add_caption_help(bot, update):
 
 
 @Client.on_message(filters.private & filters.command("settings"))
-async def settings_handler(bot: Client, m: Message):
-    await AddUser(bot, m)
-           
-    editable = await m.reply_text("**👀 Processing....**", quote=True)
-    await OpenSettings(editable, m.message)
+async def settings_handler(bot, update):
+    await update.answer()
+    upload_as_doc = await db.get_upload_as_doc(update.from_user.id)
+    if upload_as_doc:
+        await db.set_upload_as_doc(update.from_user.id, False)
+    else:
+        await db.set_upload_as_doc(update.from_user.id, True)
+    await OpenSettings(update.message)
+
