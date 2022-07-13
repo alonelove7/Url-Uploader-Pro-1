12bd57cb6597b711d9f9ef215d3f872fba01690f
TellyHub <106040852+TellyHub@users.noreply.github.com>
2022-07-13 13:40:05 +0530

    Update database.py

--- Uploader/database/database.py
+++ Uploader/database/database.py
@@ -15,10 +15,15 @@ class Database:
         return dict(
             id=id,
             join_date=datetime.date.today().isoformat(),
-            apply_caption=True,
+            ban_status=dict(
+                is_banned=False,
+                ban_duration=0,
+                banned_on=datetime.date.max.isoformat(),
+                ban_reason=''),
             upload_as_doc=False,
             thumbnail=None,
-            caption=None
+            generate_ss=False,
+            generate_sample_video=False
         )
 
     async def add_user(self, id):
@@ -36,17 +41,41 @@ class Database:
     async def get_all_users(self):
         all_users = self.col.find({})
         return all_users
+    
+    async def remove_ban(self, id):
+        ban_status = dict(
+            is_banned=False,
+            ban_duration=0,
+            banned_on=datetime.date.max.isoformat(),
+            ban_reason=''
+        )
+        await self.col.update_one({'id': id}, {'$set': {'ban_status': ban_status}})
 
+    async def ban_user(self, user_id, ban_duration, ban_reason):
+        ban_status = dict(
+            is_banned=True,
+            ban_duration=ban_duration,
+            banned_on=datetime.date.today().isoformat(),
+            ban_reason=ban_reason
+        )
+        await self.col.update_one({'id': user_id}, {'$set': {'ban_status': ban_status}})
+
+    async def get_ban_status(self, id):
+        default = dict(
+            is_banned=False,
+            ban_duration=0,
+            banned_on=datetime.date.max.isoformat(),
+            ban_reason=''
+        )
+        user = await self.col.find_one({'id': int(id)})
+        return user.get('ban_status', default)
+
+    async def get_all_banned_users(self):
+        banned_users = self.col.find({'ban_status.is_banned': True})
+        return banned_users
+    
     async def delete_user(self, user_id):
         await self.col.delete_many({'id': int(user_id)})
-
-
-    async def set_apply_caption(self, id, apply_caption):
-        await self.col.update_one({'id': id}, {'$set': {'apply_caption': apply_caption}})
-
-    async def get_apply_caption(self, id):
-        user = await self.col.find_one({'id': int(id)})
-        return user.get('apply_caption', True)
 
     async def set_upload_as_doc(self, id, upload_as_doc):
         await self.col.update_one({'id': id}, {'$set': {'upload_as_doc': upload_as_doc}})
@@ -62,16 +91,19 @@ class Database:
         user = await self.col.find_one({'id': int(id)})
         return user.get('thumbnail', None)
 
-    async def set_caption(self, id, caption):
-        await self.col.update_one({'id': id}, {'$set': {'caption': caption}})
+    async def set_generate_ss(self, id, generate_ss):
+        await self.col.update_one({'id': id}, {'$set': {'generate_ss': generate_ss}})
 
-    async def get_caption(self, id):
+    async def get_generate_ss(self, id):
         user = await self.col.find_one({'id': int(id)})
-        return user.get('caption', None)
+        return user.get('generate_ss', False)
 
-    async def get_user_data(self, id) -> dict:
+    async def set_generate_sample_video(self, id, generate_sample_video):
+        await self.col.update_one({'id': id}, {'$set': {'generate_sample_video': generate_sample_video}})
+
+    async def get_generate_sample_video(self, id):
         user = await self.col.find_one({'id': int(id)})
-        return user or None
+        return user.get('generate_sample_video', False)
 
 
 db = Database(Config.DATABASE_URL, "Url-Uploader-Bot")
