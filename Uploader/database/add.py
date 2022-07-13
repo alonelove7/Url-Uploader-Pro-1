6a6ee443b9ccb35d3970462f921b222d30dd0521
TellyHub <106040852+TellyHub@users.noreply.github.com>
2022-07-13 11:12:31 +0530

    Update add.py

--- Uploader/database/add.py
+++ Uploader/database/add.py
@@ -14,7 +14,7 @@ async def add_user_to_database(uid, fname, lname):
             "_id": uid,
             "name": f"{fname} {lname}",
         }
-        Database.mergebot.users.insert_one(userDetails)
+        Database.tellybots.users.insert_one(userDetails)
         print(f"New user added id={uid}\n{fname} {lname} \n")
     except DuplicateKeyError:
         print(f"Duplicate Entry Found for id={uid}\n{fname} {lname} \n")
