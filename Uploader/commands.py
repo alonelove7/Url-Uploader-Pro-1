a19486115ca60c691cfbad3c6ea598c8ebc4968e
TellyHub <106040852+TellyHub@users.noreply.github.com>
2022-07-13 14:35:32 +0530

    Update commands.py

--- Uploader/commands.py
+++ Uploader/commands.py
@@ -98,5 +98,20 @@ async def add_caption_help(bot, update):
     )
 
 
+@Client.on_message(
+    filters.private & filters.command("info")
+)
+async def info_handler(bot, update):
 
+
+    if update.from_user.last_name:
+        last_name = update.from_user.last_name
+    else:
+        last_name = "None"
+
+  
+    await update.reply_text(  
+        text=Translation.INFO_TEXT.format(update.from_user.first_name, last_name, update.from_user.username, update.from_user.id, update.from_user.mention, update.from_user.dc_id, update.from_user.language_code, update.from_user.status),             
+        disable_web_page_preview=True
+    )
 
