b548b228c0872c31b406c3f164d7fd71a5240cdb
TellyHub <106040852+TellyHub@users.noreply.github.com>
2022-07-14 08:51:29 +0530

    Update echo.py

--- Uploader/echo.py
+++ Uploader/echo.py
@@ -120,7 +120,7 @@ async def echo(bot, update):
         ]        
     else:
         command_to_exec = [
-            "youtube-dl",
+            "yt-dlp",
             "--no-warnings",
             "--youtube-skip-dash-manifest",
             "-j",
