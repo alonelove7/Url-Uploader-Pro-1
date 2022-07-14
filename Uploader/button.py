53cfc59499dda8a6e723f63979367ad4c5da58a4
TellyHub <106040852+TellyHub@users.noreply.github.com>
2022-07-14 13:10:53 +0530

    Update button.py

--- Uploader/button.py
+++ Uploader/button.py
@@ -142,7 +142,7 @@ async def youtube_dl_call_back(bot, update):
             
             
             "dash,m3u8:native",
-            "-F", format,
+            "-f",
 
             "--hls-prefer-ffmpeg", youtube_dl_url,
             "-o", download_directory
