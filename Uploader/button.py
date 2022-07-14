94eb1c48a1441e1c3054cb2a4ac52b73d1003920
TellyHub <106040852+TellyHub@users.noreply.github.com>
2022-07-14 08:37:53 +0530

    Update button.py

--- Uploader/button.py
+++ Uploader/button.py
@@ -1,4 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) Shrimadhav U K | @Tellybots
 
+# the logging things
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+import asyncio
+import json
+import math
+import os
+import shutil
+import time
+from datetime import datetime
+from pyrogram import enums 
+from Uploader.config import Config
+from Uploader.script import Translation
+from Uploader.thumbnail import *
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+from pyrogram.types import *
+from Uploader.functions.display_progress import progress_for_pyrogram, humanbytes
+from Uploader.database.database import db
+from PIL import Image
+from Uploader.functions.ran_text import random_char
 
 
 
