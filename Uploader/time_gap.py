b1a9a0469df1eed0218578db82f4456b88825277
TellyHub <106040852+TellyHub@users.noreply.github.com>
2022-07-15 10:51:24 +0530

    Update time_gap.py

--- Uploader/time_gap.py
+++ Uploader/time_gap.py
@@ -1,6 +1,6 @@
 import time
 from Uploader.config import Config
-from .progress_bar import TimeFormatter
+from Uploader.functions.display_progress import TimeFormatter
 
 async def timegap_check(m):
     """Checking the time gap is completed or not 
