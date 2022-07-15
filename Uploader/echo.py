4fa7c7769d26f37cbc29bd90abf6654ace486286
cvatricks <68840422+cvatricks@users.noreply.github.com>
2022-07-15 21:11:07 +0530

    Update echo.py

--- Uploader/echo.py
+++ Uploader/echo.py
@@ -128,7 +128,7 @@ async def echo(bot, update):
             url,
             "--proxy", Config.HTTP_PROXY
         ]
-    elif "/sonyliv/" in url:
+    elif "sonyliv" in url:
         command_to_exec = [
             "yt-dlp",
             "--no-warnings",
@@ -164,7 +164,7 @@ async def echo(bot, update):
     t_response = stdout.decode().strip()
     # logger.info(t_response)
 
-    if (e_response and "nonnumeric port" not in e_response) and "/sonyliv/" not in url:
+    if (e_response and "nonnumeric port" not in e_response) and "sonyliv" not in url:
         # logger.warn("Status : FAIL", exc.returncode, exc.output)
         error_message = e_response.replace("please report this issue on https://yt-dl.org/bug . Make sure you are using the latest version; see  https://yt-dl.org/update  on how to update. Be sure to call youtube-dl with the --verbose flag and include its complete output.", "")
         if "This video is only available for registered users." in error_message:
