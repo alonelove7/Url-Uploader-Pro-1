aa41b10365795e22ab6f9ed4a58f1bcc67bde709
TellyHub <106040852+TellyHub@users.noreply.github.com>
2022-07-16 17:03:45 +0530

    Update commands.py

--- Uploader/commands.py
+++ Uploader/commands.py
@@ -101,9 +101,7 @@ async def info_handler(bot, update):
     if update.from_user.last_name:
         last_name = update.from_user.last_name
     else:
-        last_name = "None"
-
-  
+        last_name = "None" 
     await update.reply_text(  
         text=Translation.INFO_TEXT.format(update.from_user.first_name, last_name, update.from_user.username, update.from_user.id, update.from_user.mention, update.from_user.dc_id, update.from_user.language_code, update.from_user.status), 
         reply_markup=Translation.BUTTONS,           
