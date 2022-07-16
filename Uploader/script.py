67d1da20eecd95615d108ddd7acf5ca4dd6fc481
TellyHub <106040852+TellyHub@users.noreply.github.com>
2022-07-16 17:13:50 +0530

    Update script.py

--- Uploader/script.py
+++ Uploader/script.py
@@ -204,7 +204,7 @@ You can use /rename command after receiving file to rename it with custom thumbn
     CANCEL_STR = "Process Cancelled"
     ZIP_UPLOADED_STR = "Uploaded {} files in {} seconds"
     FREE_USER_LIMIT_Q_SZE = """Cannot Process.
-Free users only 1 request per 30 minutes.
+Free users only 1 request per 2 hours.
 Please Check /plans and upgrade your subscription """
     SLOW_URL_DECED = "Gosh that seems to be a very slow URL. Since you were screwing my home, I am in no mood to download this file. Meanwhile, why don't you try this:==> https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
     FORCE_SUBSCRIBE_TEXT = "<code>Sorry Dear You Must Join My Updates Channel for using me 😌😉....</code>"
