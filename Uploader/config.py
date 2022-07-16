b6679dd17da0e16b90a9228a4ec877b89e58a2b4
TellyHub <106040852+TellyHub@users.noreply.github.com>
2022-07-16 17:06:17 +0530

    Update config.py

--- Uploader/config.py
+++ Uploader/config.py
@@ -41,7 +41,7 @@ class Config(object):
     # maximum message length in Telegram
     MAX_MESSAGE_LENGTH = 4096
     # set timeout for subprocess
-    PROCESS_MAX_TIMEOUT = 3600
+    PROCESS_MAX_TIMEOUT = 7200
     # watermark file
     DEF_WATER_MARK_FILE = ""
     DATABASE_URL = os.environ.get("DATABASE_URL", "")
