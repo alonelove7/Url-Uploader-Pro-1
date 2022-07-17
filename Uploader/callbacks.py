a1cf5e302482647a54d9ec02ec42c07aa53dc44c
TellyHub <106040852+TellyHub@users.noreply.github.com>
2022-07-17 10:40:21 +0530

    Update callbacks.py

--- Uploader/callbacks.py
+++ Uploader/callbacks.py
@@ -4,7 +4,7 @@ from Uploader.config import Config
 from Uploader.dl_button import ddl_call_back
 from Uploader.button import youtube_dl_call_back
 
-from Uploader.script import Translation
+from Uploader.script import Translation, TEXT
 from pyrogram import Client, types
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
@@ -12,48 +12,78 @@ import logging
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
+from Uploader.commands import *
+from Uploader.thumbnail import delete_thumbnail
 
 
 
 
 
-@Client.on_callback_query()
-async def button(bot, update):
-    if update.data == "home":
-        await update.message.edit(
-            text=Translation.START_TEXT.format(update.from_user.mention),
-            reply_markup=Translation.START_BUTTONS,
-            #disable_web_page_preview=True
-        )
-    elif update.data == "help":
-        await update.message.edit(
-            text=Translation.HELP_TEXT,
-            reply_markup=Translation.HELP_BUTTONS,
-            #disable_web_page_preview=True
-        )
-    elif update.data == "about":
-        await update.message.edit(
-            text=Translation.ABOUT_TEXT,
-            reply_markup=Translation.ABOUT_BUTTONS,
-            #disable_web_page_preview=True
-        )
-
-    elif update.data == "setThumbnail":
-        await update.message.edit(
-            text=Translation.TEXT,
-            reply_markup=Translation.BUTTONS,
-            #disable_web_page_preview=True
-        )
 
 
-    elif "close" in update.data:
-        await update.message.delete(True)
 
-    elif "|" in update.data:
-        await youtube_dl_call_back(bot, update)
-    elif "=" in update.data:
-        await ddl_call_back(bot, update)
 
-    else:
-        await update.message.delete()
 
+
+@Client.on_callback_query(filters.regex('^help$'))
+async def help_cb(c, m):
+    await m.answer()
+    await help(c, m, True)
+
+
+@Client.on_callback_query(filters.regex('^donate$'))
+async def donate(c, m):
+    button = [[
+        InlineKeyboardButton('🏕️ Home', callback_data='back'),
+        InlineKeyboardButton('📘 About', callback_data='about')
+        ],[
+        InlineKeyboardButton('❌ Close', callback_data='close')
+    ]]
+    reply_markup = InlineKeyboardMarkup(button)
+    await m.answer()
+    await m.message.edit(
+        text=TEXT.DONATE_USER.format(m.from_user.first_name),
+        disable_web_page_preview=True,
+        reply_markup=reply_markup
+    )
+
+
+@Client.on_callback_query(filters.regex('^close$'))
+async def close_cb(c, m):
+    try:
+        await m.message.delete()
+        await m.message.reply_to_message.delete()
+    except:
+        pass
+
+
+@Client.on_callback_query(filters.regex('^back$'))
+async def back_cb(c, m):
+    await m.answer()
+    await start(c, m, True)
+
+
+@Client.on_callback_query(filters.regex('^about$'))
+async def about_cb(c, m):
+    await m.answer()
+    await about(c, m, True)
+
+@Client.on_callback_query(filters.regex('^|'))
+async def about_cb(c, m):
+    await m.answer()               
+    await youtube_dl_call_back(c, m)
+
+
+
+@Client.on_callback_query(filters.regex('^='))
+async def about_cb(c, m):
+    await m.answer()               
+    await ddl_call_back(c, m)
+
+
+
+@Client.on_callback_query(filters.regex('^del$'))
+async def deletethumb_cb(c, m):
+    await m.answer()
+    await delete_thumbnail(c, m.message.reply_to_message)
+    await m.message.delete
