d26c4db97a2e0ad3731e9da86af7c33a8122fa15
TellyHub <106040852+TellyHub@users.noreply.github.com>
2022-07-17 09:25:43 +0530

    Update callbacks.py

--- Uploader/callbacks.py
+++ Uploader/callbacks.py
@@ -3,11 +3,11 @@ from Uploader.functions.display_progress import progress_for_pyrogram, humanbyte
 from Uploader.config import Config
 from Uploader.dl_button import ddl_call_back
 from Uploader.button import youtube_dl_call_back
-from Uploader.settings.settings import OpenSettings
+
 from Uploader.script import Translation
 from pyrogram import Client, types
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from Uploader.database.database import db
+
 import logging
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -37,24 +37,7 @@ async def button(bot, update):
             reply_markup=Translation.ABOUT_BUTTONS,
             #disable_web_page_preview=True
         )
-    elif update.data == "OpenSettings":
-        await update.answer()
-        await OpenSettings(update.message)
-    elif update.data == "showThumbnail":
-        thumbnail = await db.get_thumbnail(update.from_user.id)
-        if not thumbnail:
-            await update.answer("You didn't set any custom thumbnail!", show_alert=True)
-        else:
-            await update.answer()
-            await bot.send_photo(update.message.chat.id, thumbnail, "Custom Thumbnail",
-                               reply_markup=types.InlineKeyboardMarkup([[
-                                   types.InlineKeyboardButton("Delete Thumbnail",
-                                                              callback_data="deleteThumbnail")
-                               ]]))
-    elif update.data == "deleteThumbnail":
-        await db.set_thumbnail(update.from_user.id, None)
-        await update.answer("Okay, I deleted your custom thumbnail. Now I will apply default thumbnail.", show_alert=True)
-        await update.message.delete(True)
+
     elif update.data == "setThumbnail":
         await update.message.edit(
             text=Translation.TEXT,
@@ -62,14 +45,7 @@ async def button(bot, update):
             #disable_web_page_preview=True
         )
 
-    elif update.data == "triggerUploadMode":
-        await update.answer()
-        upload_as_doc = await db.get_upload_as_doc(update.from_user.id)
-        if upload_as_doc:
-            await db.set_upload_as_doc(update.from_user.id, False)
-        else:
-            await db.set_upload_as_doc(update.from_user.id, True)
-        await OpenSettings(update.message)
+
     elif "close" in update.data:
         await update.message.delete(True)
 
